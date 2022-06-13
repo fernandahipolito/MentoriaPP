171c6319a9b1dce974f19fbdb0f9b15012025323
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-06-13 10:53:04 -0300

    adjust configuration files

---
+++ app/src/main/java/com/example/projetomentoria/secondexercise/SecondExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projetomentoria.secondexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_exercise)
+    }
+}
