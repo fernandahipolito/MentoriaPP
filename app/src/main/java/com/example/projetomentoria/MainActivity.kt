6cb49f52b8787bc050ea14d98d41d6f04a7cb589
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-06-13 10:55:28 -0300

    adjust main activity and second exercise

--- app/src/main/java/com/example/projetomentoria/MainActivity.kt
+++ app/src/main/java/com/example/projetomentoria/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.projetomentoria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.example.projetomentoria.databinding.ActivityMainBinding
+import com.example.projetomentoria.exerciseone.FirstExerciseActivity
+import com.example.projetomentoria.secondexercise.SecondExerciseActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.appCompatButton.setOnClickListener {
+            val intent = Intent(this, FirstExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.appCompatButton2.setOnClickListener {
+
+            val intent2 = Intent(this, SecondExerciseActivity::class.java)
+            startActivity(intent2)
+        }
     }
 }
--- app/src/main/java/com/example/projetomentoria/secondexercise/SecondExerciseActivity.kt
+++ app/src/main/java/com/example/projetomentoria/secondexercise/SecondExerciseActivity.kt
@@ -2,10 +2,37 @@ package com.example.projetomentoria.secondexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projetomentoria.R
+import com.example.projetomentoria.databinding.ActivitySecondExerciseBinding
+
 
 class SecondExerciseActivity : AppCompatActivity() {
+
+    val binding: ActivitySecondExerciseBinding by lazy {
+        ActivitySecondExerciseBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_exercise)
+        setContentView(binding.root)
+
+        setClickListener()
+    }
+
+    fun clickCount(): Int {
+        var number = 0
+        with(binding) {
+            number = appCompatTextView.text.toString().toInt()
+            number++
+        }
+        return number
+    }
+
+    private fun setClickListener() {
+        binding.counterButton.setOnClickListener {
+            binding.appCompatTextView.text = clickCount().toString()
+        }
     }
 }
