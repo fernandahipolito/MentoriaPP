4f3500a1d07c44546cdd3d6d2961f199f5b4d9df
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-06-20 17:34:55 -0300

    config viewModel and add third exercise

---
+++ app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projetomentoria.thirdexercise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetomentoria.R
+
+class ThirdExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_exercise)
+    }
+}
