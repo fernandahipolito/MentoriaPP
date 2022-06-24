128e140c5d399deeb70aab2bbcf82ab2edf9215f
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-06-24 10:23:29 -0300

    first commit of third exercise

--- app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
+++ app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
@@ -1,12 +1,24 @@
 package com.example.projetomentoria.thirdexercise
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projetomentoria.R
 
 class ThirdExerciseActivity : AppCompatActivity() {
+    var number: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_exercise)
+
+       val txtCounter = findViewById<TextView>(R.id.appCompatTextView)
+       val btnIncrement = findViewById<Button>(R.id.counterButton)
+
+        btnIncrement.setOnClickListener {
+            number++
+            txtCounter.text = "$number"
+        }
     }
 }
