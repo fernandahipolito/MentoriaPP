2463e1db0a3893acf97de1fd3044339bef9335f5
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-06-29 07:37:41 -0300

    create third exercise activity

--- app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
+++ app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
@@ -3,22 +3,33 @@ package com.example.projetomentoria.thirdexercise
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projetomentoria.R
 
+
 class ThirdExerciseActivity : AppCompatActivity() {
-    var number: Int = 0
+    private val model: ThirdExerciseViewModel by viewModels()
+    //var number: Int = 0 -> setei o valor 0 na viewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_exercise)
 
        val txtCounter = findViewById<TextView>(R.id.appCompatTextView)
        val btnIncrement = findViewById<Button>(R.id.counterButton)
 
-        btnIncrement.setOnClickListener {
-            number++
+        model.valueLiveData.observe(this) { number ->
             txtCounter.text = "$number"
+
+
+        }
+        btnIncrement.setOnClickListener {
+            model.increment()
+           // number++
         }
     }
 }
