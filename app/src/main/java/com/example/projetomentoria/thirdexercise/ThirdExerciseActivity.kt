88d498af48251bfd16ef5d92fc2551e56a84576e
fernandahipolito <fernanda_hipolito@hotmail.com>
2022-07-13 16:05:59 -0300

    correct the third exercise

--- app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
+++ app/src/main/java/com/example/projetomentoria/thirdexercise/ThirdExerciseActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
-
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projetomentoria.R
 
@@ -13,23 +12,19 @@ class ThirdExerciseActivity : AppCompatActivity() {
     private val model: ThirdExerciseViewModel by viewModels()
     //var number: Int = 0 -> setei o valor 0 na viewModel
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_exercise)
 
-       val txtCounter = findViewById<TextView>(R.id.appCompatTextView)
-       val btnIncrement = findViewById<Button>(R.id.counterButton)
+        val txtCounter = findViewById<TextView>(R.id.appCompatTextView)
+        val btnIncrement = findViewById<Button>(R.id.counterButton)
 
         model.valueLiveData.observe(this) { number ->
             txtCounter.text = "$number"
-
-
         }
+
         btnIncrement.setOnClickListener {
             model.increment()
-           // number++
         }
     }
 }
